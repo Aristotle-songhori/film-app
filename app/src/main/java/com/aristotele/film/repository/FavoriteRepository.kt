af0a06ca4221dd8e4a24e87b28e0ef6056eef95f
Aristotele Songhori <aristotele.songhori@gmail.com>
2022-12-17 12:52:23 +0100

    ساختن فرگمت علاقه مندی ها درس 70

---
+++ app/src/main/java/com/aristotele/film/repository/FavoriteRepository.kt
@@ -0,0 +1,10 @@
+package com.aristotele.film.repository
+
+
+import com.aristotele.film.db.MoviesDao
+import javax.inject.Inject
+
+class FavoriteRepository @Inject constructor(private val dao: MoviesDao) {
+
+    fun allFavoriteList() = dao.getAllMovies()
+}
